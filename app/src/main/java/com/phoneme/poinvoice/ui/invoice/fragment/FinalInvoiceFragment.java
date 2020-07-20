9ac88d841efeb3ccd82b1e5ca5f31be0ffb5ad33
phoneme2015 <50018309+phoneme2015@users.noreply.github.com>
2020-07-20 11:11:13 +0530

    pdf not working

--- app/src/main/java/com/phoneme/poinvoice/ui/invoice/fragment/FinalInvoiceFragment.java
+++ app/src/main/java/com/phoneme/poinvoice/ui/invoice/fragment/FinalInvoiceFragment.java
@@ -1,17 +1,28 @@
 package com.phoneme.poinvoice.ui.invoice.fragment;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.phoneme.poinvoice.R;
 import com.phoneme.poinvoice.ui.invoice.InvoiceViewModel;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class FinalInvoiceFragment extends Fragment {
 
@@ -26,4 +37,83 @@ public class FinalInvoiceFragment extends Fragment {
 
         return root;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+//    private void createPdf() {
+//
+//        PdfDocument document = new PdfDocument();
+//        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(b.getWidth(), b.getHeight(), 1).create();
+//        PdfDocument.Page page = document.startPage(pageInfo);
+//
+//        Canvas canvas = page.getCanvas();
+//
+//
+//        Paint paint = new Paint();
+//        paint.setColor(Color.parseColor("#ffffff"));
+//        canvas.drawPaint(paint);
+//
+//
+//        Bitmap bitmap = Bitmap.createScaledBitmap(b, b.getWidth(), b.getHeight(), true);
+//
+//        paint.setColor(Color.BLUE);
+//        canvas.drawBitmap(bitmap, 0, 0, null);
+//        document.finishPage(page);
+//        File filePath = new File(path);
+//        try {
+//            document.writeTo(new FileOutputStream(filePath));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            Toast.makeText(this, "Something wrong: " + e.toString(), Toast.LENGTH_LONG).show();
+//        }
+//
+//        // close the document
+//        document.close();
+//
+//        openPdf(path);// You can open pdf after complete
+//    }
+//
+//    private void takeScreenShot() {
+//
+//        File folder = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Signature/");
+//
+//        if (!folder.exists()) {
+//            boolean success = folder.mkdir();
+//        }
+//
+//        path = folder.getAbsolutePath();
+//        path = path + "/" + signature_pdf_ + System.currentTimeMillis() + ".pdf";// path where pdf will be stored
+//
+//        View u = findViewById(R.id.scroll);
+//        NestedScrollView z = (NestedScrollView) findViewById(R.id.scroll); // parent view
+//        totalHeight = z.getChildAt(0).getHeight();// parent view height
+//        totalWidth = z.getChildAt(0).getWidth();// parent view width
+//
+//        //Save bitmap to  below path
+//        String extr = Environment.getExternalStorageDirectory() + "/Signature/";
+//        File file = new File(extr);
+//        if (!file.exists())
+//            file.mkdir();
+//        String fileName = signature_img_ + ".jpg";
+//        myPath = new File(extr, fileName);
+//        imagesUri = myPath.getPath();
+//        FileOutputStream fos = null;
+//        b = getBitmapFromView(u, totalHeight, totalWidth);
+//
+//        try {
+//            fos = new FileOutputStream(myPath);
+//            b.compress(Bitmap.CompressFormat.PNG, 100, fos);
+//            fos.flush();
+//            fos.close();
+//
+//        } catch (Exception e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//        createPdf();// create pdf after creating bitmap and saving
+//
+//    }
 }
