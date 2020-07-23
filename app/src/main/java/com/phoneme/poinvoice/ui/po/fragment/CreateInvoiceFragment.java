6c0555caa25eb2a270178272607a65014260f595
phoneme2015 <50018309+phoneme2015@users.noreply.github.com>
2020-07-23 17:04:05 +0530

    Basic working. But UI needs to be improved

--- app/src/main/java/com/phoneme/poinvoice/ui/po/fragment/CreateInvoiceFragment.java
+++ app/src/main/java/com/phoneme/poinvoice/ui/po/fragment/CreateInvoiceFragment.java
@@ -1,13 +1,22 @@
 package com.phoneme.poinvoice.ui.po.fragment;
 
 import android.app.DatePickerDialog;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -24,6 +33,8 @@ public class CreateInvoiceFragment extends Fragment {
     private EditText dob;
     Calendar myCalendar;
     DatePickerDialog.OnDateSetListener date;
+    private Button serviceButton;
+    private LinearLayout servicelayout;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -39,6 +50,14 @@ public class CreateInvoiceFragment extends Fragment {
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
         dob=(EditText)view.findViewById(R.id.invoice_date);
         dob.setText(sdf.format(myCalendar.getTime()));
+        serviceButton=(Button)view.findViewById(R.id.service_button);
+        servicelayout=(LinearLayout)view.findViewById(R.id.service_products_layout);
+        serviceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createView();
+            }
+        });
         date = new DatePickerDialog.OnDateSetListener() {
 
             @Override
@@ -79,5 +98,224 @@ public class CreateInvoiceFragment extends Fragment {
                 // Toast.makeText(getApplicationContext(), "In On click", Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    private void createView(){
+
+        View v = new View(getContext());
+        v.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                5
+        ));
+
+        v.setBackgroundColor(Color.parseColor("#B3B3B3"));
+
+
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams (
+                (int) getResources().getDimension(R.dimen.edit_width),
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+
+        RelativeLayout relativeLayout=new RelativeLayout(getContext());
+        RelativeLayout.LayoutParams params2 = new RelativeLayout.LayoutParams (
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        RelativeLayout.LayoutParams paramsLeft = new RelativeLayout.LayoutParams (
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        //paramsLeft.bottomMargin=(int) getResources().getDimension(R.dimen.margin);
+
+        //relativeLayout.setBackgroundColor((int)getResources().getColor(R.color.colorAccent));
+        relativeLayout.setLayoutParams(params2);
+//        relativeLayout.getLayoutParams().width=(int) getResources().getDimension(R.dimen.edit_width);
+//        relativeLayout.getLayoutParams().height=RelativeLayout.LayoutParams.WRAP_CONTENT;
+//
+        TextView textView=new TextView(getContext());
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP,12);
+        textView.setHint("Service");
+        textView.setLayoutParams(paramsLeft);
+
+        EditText edittTxt = new EditText(getContext());
+        //edittTxt.setTextSize((int)getResources().getDimension(R.dimen.text_size));
+        edittTxt.setTextSize(TypedValue.COMPLEX_UNIT_SP,12);
+        edittTxt.setHint("Service");
+        edittTxt.setHintTextColor((int)getResources().getColor(R.color.colorAccent));
+//
+//
+        edittTxt.setWidth((int) getResources().getDimension(R.dimen.edit_width));
+        relativeLayout.addView(v);
+        relativeLayout.addView(edittTxt);
+        relativeLayout.addView(textView);
+
+//
+       params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        edittTxt.setLayoutParams(params);
+        edittTxt.setBackground(getContext().getResources().getDrawable(R.drawable.edit_bg));
+//
+        servicelayout.addView(relativeLayout);
+
+
+
+
+
+
+
+        View vDescription = new View(getContext());
+        vDescription.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                5
+        ));
+
+        vDescription.setBackgroundColor(Color.parseColor("#B3B3B3"));
+
+
+        RelativeLayout.LayoutParams paramsDescription = new RelativeLayout.LayoutParams (
+                (int) getResources().getDimension(R.dimen.edit_width),
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+
+        RelativeLayout relativeLayoutDescription=new RelativeLayout(getContext());
+        RelativeLayout.LayoutParams paramsDescription2 = new RelativeLayout.LayoutParams (
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        RelativeLayout.LayoutParams paramsDescriptionLeft = new RelativeLayout.LayoutParams (
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        relativeLayoutDescription.setLayoutParams(paramsDescription2);
+
+
+        TextView textViewDescription=new TextView(getContext());
+        textViewDescription.setTextSize(TypedValue.COMPLEX_UNIT_SP,12);
+        textViewDescription.setHint("Description");
+        textViewDescription.setLayoutParams(paramsDescriptionLeft);
+
+        EditText edittTxtDescription = new EditText(getContext());
+
+        edittTxtDescription.setTextSize(TypedValue.COMPLEX_UNIT_SP,12);
+        edittTxtDescription.setHint("Description");
+        edittTxtDescription.setHintTextColor((int)getResources().getColor(R.color.colorAccent));
+
+        edittTxtDescription.setWidth((int) getResources().getDimension(R.dimen.edit_width));
+        relativeLayoutDescription.addView(vDescription);
+        relativeLayoutDescription.addView(edittTxtDescription);
+        relativeLayoutDescription.addView(textViewDescription);
+
+        paramsDescription.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        edittTxtDescription.setLayoutParams(paramsDescription);
+        edittTxtDescription.setBackground(getContext().getResources().getDrawable(R.drawable.edit_bg));
+
+        servicelayout.addView(relativeLayoutDescription);
+
+
+
+
+
+//Quantity
+
+        View vQuantity = new View(getContext());
+        vQuantity.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                5
+        ));
+
+        vQuantity.setBackgroundColor(Color.parseColor("#B3B3B3"));
+
+
+        RelativeLayout.LayoutParams paramsQuantity = new RelativeLayout.LayoutParams (
+                (int) getResources().getDimension(R.dimen.edit_width),
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+
+        RelativeLayout relativeLayoutQuantity=new RelativeLayout(getContext());
+        RelativeLayout.LayoutParams paramsQuantity2 = new RelativeLayout.LayoutParams (
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+
+        RelativeLayout.LayoutParams paramsQuantityLeft = new RelativeLayout.LayoutParams (
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        relativeLayoutQuantity.setLayoutParams(paramsQuantity2);
+
+
+        TextView textViewQuantity=new TextView(getContext());
+        textViewQuantity.setTextSize(TypedValue.COMPLEX_UNIT_SP,12);
+        textViewQuantity.setHint("Quantity");
+        textViewQuantity.setLayoutParams(paramsQuantityLeft);
+
+        EditText edittTxtQuantity = new EditText(getContext());
+
+        edittTxtQuantity.setTextSize(TypedValue.COMPLEX_UNIT_SP,12);
+        edittTxtQuantity.setHint("Quantity");
+        edittTxtQuantity.setHintTextColor((int)getResources().getColor(R.color.colorAccent));
+
+        edittTxtQuantity.setWidth((int) getResources().getDimension(R.dimen.edit_width));
+        relativeLayoutQuantity.addView(vQuantity);
+        relativeLayoutQuantity.addView(edittTxtQuantity);
+        relativeLayoutQuantity.addView(textViewQuantity);
+
+        paramsQuantity.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        edittTxtQuantity.setLayoutParams(paramsDescription);
+        edittTxtQuantity.setBackground(getContext().getResources().getDrawable(R.drawable.edit_bg));
+
+        servicelayout.addView(relativeLayoutQuantity);
+
+
+
+
+        //Price
+
+        View vPrice = new View(getContext());
+        vPrice.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                5
+        ));
+
+        vPrice.setBackgroundColor(Color.parseColor("#B3B3B3"));
+
+
+        RelativeLayout.LayoutParams paramsPrice = new RelativeLayout.LayoutParams (
+                (int) getResources().getDimension(R.dimen.edit_width),
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+
+        RelativeLayout relativeLayoutPrice=new RelativeLayout(getContext());
+        RelativeLayout.LayoutParams paramsPrice2 = new RelativeLayout.LayoutParams (
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+
+        RelativeLayout.LayoutParams paramsPriceLeft = new RelativeLayout.LayoutParams (
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        relativeLayoutPrice.setLayoutParams(paramsPrice2);
+
+
+        TextView textViewPrice=new TextView(getContext());
+        textViewPrice.setTextSize(TypedValue.COMPLEX_UNIT_SP,12);
+        textViewPrice.setHint("Price");
+        textViewPrice.setLayoutParams(paramsPriceLeft);
+
+        EditText edittTxtPrice = new EditText(getContext());
+
+        edittTxtPrice.setTextSize(TypedValue.COMPLEX_UNIT_SP,12);
+        edittTxtPrice.setHint("Price");
+        edittTxtPrice.setHintTextColor((int)getResources().getColor(R.color.colorAccent));
+
+        edittTxtPrice.setWidth((int) getResources().getDimension(R.dimen.edit_width));
+        relativeLayoutPrice.addView(vPrice);
+        relativeLayoutPrice.addView(edittTxtPrice);
+        relativeLayoutPrice.addView(textViewPrice);
+
+        paramsPrice.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        edittTxtPrice.setLayoutParams(paramsDescription);
+        edittTxtPrice.setBackground(getContext().getResources().getDrawable(R.drawable.edit_bg));
+
+        servicelayout.addView(relativeLayoutPrice);
     }
 }
