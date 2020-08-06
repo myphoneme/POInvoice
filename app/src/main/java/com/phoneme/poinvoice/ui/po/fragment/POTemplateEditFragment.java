835ceff6b4c8d0636ea907b524017ef23db271ab
phoneme2015 <50018309+phoneme2015@users.noreply.github.com>
2020-08-06 09:47:51 +0530

    State data is coming on po edit template

--- app/src/main/java/com/phoneme/poinvoice/ui/po/fragment/POTemplateEditFragment.java
+++ app/src/main/java/com/phoneme/poinvoice/ui/po/fragment/POTemplateEditFragment.java
@@ -18,16 +18,27 @@ import com.phoneme.poinvoice.interfaces.GetDataService;
 import com.phoneme.poinvoice.ui.po.model.PoTemplateDataModel;
 import com.phoneme.poinvoice.ui.po.network.PoTemplateEditGETResponse;
 import com.phoneme.poinvoice.ui.po.network.PoTemplateListResponse;
+import java.util.List;
+import java.util.ArrayList;
+import android.widget.Toast;
+import com.phoneme.poinvoice.ui.po.model.StateModelData;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class POTemplateEditFragment extends Fragment {
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
-    private Button submit;
+public class POTemplateEditFragment extends Fragment implements
+        AdapterView.OnItemSelectedListener{
+
+    private Button submit,Reset;
     private EditText date,PONumber,POAmount,Title,TemplateName,AddressLine1,AddressLine2,AddressLine3,GstNo;
     private  Spinner spin;
+    private ArrayList<String> states=new ArrayList<String>();
+    private PoTemplateDataModel dataModel;
+    private List<StateModelData> stateModelDataList;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_edit_po_template, container, false);
@@ -46,12 +57,20 @@ public class POTemplateEditFragment extends Fragment {
         AddressLine3=(EditText)view.findViewById(R.id.address_line3);
         GstNo=(EditText)view.findViewById(R.id.gstn_no);
         spin = (Spinner) view.findViewById(R.id.spinner_state);
+        //spin.setOnItemSelectedListener();
 
 
 //        date=(EditText)view.findViewById(R.id.po_date);
 //        PONumber=(EditText)view.findViewById(R.id.po_number);
 //        POAmount=(EditText)view.findViewById(R.id.po_amount);
         submit=(Button)view.findViewById(R.id.submit);
+        Reset=(Button)view.findViewById(R.id.reset);
+        Reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setOriginalData( dataModel,stateModelDataList);
+            }
+        });
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -99,7 +118,10 @@ public class POTemplateEditFragment extends Fragment {
         call.enqueue(new Callback<PoTemplateEditGETResponse>() {
             @Override
             public void onResponse(Call<PoTemplateEditGETResponse> call, Response<PoTemplateEditGETResponse> response) {
-                setOriginalData(response.body().getPo_templatedata());
+                dataModel=response.body().getPo_templatedata();
+                stateModelDataList=response.body().getStates();
+                setStates(response.body().getStates());
+                setOriginalData(dataModel,stateModelDataList);
             }
 
             @Override
@@ -108,12 +130,46 @@ public class POTemplateEditFragment extends Fragment {
             }
         });
     }
-    private void setOriginalData(PoTemplateDataModel dataModel){
+    private void setStates(List<StateModelData> stateModelDataList){
+        for(int i=0;i<stateModelDataList.size();i++) {
+            if (stateModelDataList.get(i).getState_Name()!=null){
+                states.add(stateModelDataList.get(i).getState_Name());
+
+            }
+        }
+    }
+    private void setOriginalData(PoTemplateDataModel dataModel,List<StateModelData> stateModelDataList){
         Title.setText(dataModel.getTitle());
         TemplateName.setText(dataModel.getTemplate_name());
         AddressLine1.setText(dataModel.getAddress_line1());
         AddressLine2.setText(dataModel.getAddress_line2());
         AddressLine3.setText(dataModel.getAddress_line3());
         GstNo.setText(dataModel.getGSTN_NO());
+        String state=dataModel.getState();
+        int statePosition=0;
+        for(int i=0;i<stateModelDataList.size();i++) {
+            if (stateModelDataList.get(i).getState_Name()!=null){
+                if(state.equalsIgnoreCase(stateModelDataList.get(i).getState_Name())){
+                    statePosition=i;
+                }
+            }
+        }
+
+        ArrayAdapter aa = new ArrayAdapter(getContext(),android.R.layout.simple_spinner_item,states);
+
+//        aa.
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        //Setting the ArrayAdapter data on the Spinner
+        spin.setAdapter(aa);
+        spin.setSelection(statePosition);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+        //Toast.makeText(this,country[position] , Toast.LENGTH_LONG).show();
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
     }
 }
