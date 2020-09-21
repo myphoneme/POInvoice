3135d4bf637d69b6011e10fb91aee72af9fa6b20
phoneme2015 <50018309+phoneme2015@users.noreply.github.com>
2020-09-21 13:43:09 +0530

    Basic UI done

--- app/src/main/java/com/phoneme/poinvoice/ui/invoice/adapter/ClientListAdapter.java
+++ app/src/main/java/com/phoneme/poinvoice/ui/invoice/adapter/ClientListAdapter.java
@@ -47,7 +47,7 @@ public class ClientListAdapter extends RecyclerView.Adapter<ClientListAdapter.Vi
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        View view = LayoutInflater.from(mcontext).inflate(R.layout.adapter_client_list, viewGroup, false);
+        View view = LayoutInflater.from(mcontext).inflate(R.layout.adapter_client_list_new_ui, viewGroup, false);
         return new ViewHolder(view);
     }
 
